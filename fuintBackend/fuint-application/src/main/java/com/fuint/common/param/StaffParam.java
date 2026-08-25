0d02194d5d4fd1522e8a50cb64ee6a3c623e60fc
fushengqian <fushengqian@qq.com>
2026-08-25 10:28:48 +0800

    fixed 餐饮版员工信息完善

--- fuintBackend/fuint-application/src/main/java/com/fuint/common/param/StaffParam.java
+++ fuintBackend/fuint-application/src/main/java/com/fuint/common/param/StaffParam.java
@@ -3,17 +3,15 @@ package com.fuint.common.param;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import java.io.Serializable;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
- * 员工请求参数
+ * 订单列表请求参数
  *
  * Created by FSQ
  * CopyRight https://www.fuint.cn
  */
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class StaffParam implements Serializable {
+public class StaffParam extends PageParam implements Serializable {
 
     @ApiModelProperty(value="ID", name="id")
     private Integer id;
@@ -32,6 +30,9 @@ public class StaffParam implements Serializable {
 
     @ApiModelProperty(value="真实姓名", name="realName")
     private String realName;
+
+    @ApiModelProperty(value="头像", name="avatar")
+    private String avatar;
 
     @ApiModelProperty(value="备注信息", name="description")
     private String description;
